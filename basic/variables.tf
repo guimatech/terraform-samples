54e7e990a48d098906243ca51607f2ff69050679
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2025-04-23 21:11:12 -0300

    tipos de variaveis

--- basic/variables.tf
+++ basic/variables.tf
@@ -1,3 +1,12 @@
 variable "file_content" {
   default = "Esse é o valor padrão da variável file_content"
+  description = "O conteúdo do arquivo a ser criado"
+  type        = string
+}
+
+variable "var_bool" {
+  default = true
+  description = "Um valor booleano de exemplo"
+  type        = bool
+  
 }
