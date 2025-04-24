fa092dca024421af515c57e7727ce6fdbf11d91b
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2025-04-24 09:37:34 -0300

    variaveis tipos complexos

--- basic/main.tf
+++ basic/main.tf
@@ -9,5 +9,18 @@ terraform {
 
 resource "local_file" "exemplo" {
   filename = "exemplo.txt"
-  content  = "Valor string: ${var.file_content} Valor boolean: ${var.var_bool}" 
+  content  = <<EOF
+Valor string: ${var.file_content} Valor boolean: ${var.var_bool}
+  
+Fruits: ${var.fruits[1]}
+
+Name: ${var.person_map["name"]}
+Idade: ${var.person_map["age"]}
+
+Name: ${var.person_tuple[0]}
+Idade: ${var.person_tuple[1]}
+
+Name: ${var.person_object.name}
+Idade: ${var.person_object.age}
+EOF 
 }
--- basic/variables.tf
+++ basic/variables.tf
@@ -8,5 +8,38 @@ variable "var_bool" {
   default = true
   description = "Um valor booleano de exemplo"
   type        = bool
+}
+
+variable "fruits" {
+  default = ["banana", "maçã", "laranja"]
+  description = "Uma lista de frutas"
+  type        = list(string)
+}
+
+variable "person_map" {
+  default = {
+    name = "João"
+    age  = 30
+  }
+  description = "Um mapa com informações de uma pessoa"
+  type        = map(string) 
+}
+
+variable "person_tuple" {
+  default = ["Maria", 25]
+  description = "Uma tupla com informações de uma pessoa"
+  type        = tuple([string, number]) 
+}
+
+variable "person_object" {
+  default = {
+    name = "Carlos"
+    age  = 40
+  }
+  description = "Um objeto com informações de uma pessoa"
+  type        = object({
+    name = string
+    age  = number
+  })
   
 }
