264fdbdf6e07e2d4b5e7b2b804128d9539e18f8c
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2025-05-09 23:33:10 -0300

    cria envs e secrets, e acessa via console o arquivo criado

--- aws/main.tf
+++ aws/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+data "aws_secretsmanager_secret" "secret" {
+  name = "arn:aws:secretsmanager:us-west-2:358417859610:secret:prod/Terraform/Db-YD63mw"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
+}
+
 provider "aws" {
   region = "us-west-2"
   profile = "default"
@@ -26,6 +34,13 @@ resource "aws_instance" "example_instance" {
   ami           = "ami-07b0c09aab6e66ee9"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.example_subnet.id
+  vpc_security_group_ids = [ aws_security_group.example_sg.id ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              DB_STRING="Server=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]};DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["DB"]}"
+              echo $DB_STRING > test.txt
+              EOF
 
   tags = {
     Name = "ExampleInstance"
@@ -49,6 +64,43 @@ resource "aws_ssm_parameter" "parameter" {
 
 output "private_dns" {
   value = aws_instance.example_instance.private_dns
+}
+
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+}
+
+resource "aws_route_table_association" "example_route_table_association" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_route_table.id
+}
+
+resource "aws_security_group" "example_sg" {
+  vpc_id = aws_vpc.example_vpc.id
+  name = "Allow SSH"
+  
+  tags = {
+    Name = "Allow SSH"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "example_sg_ingress_rule" {
+  security_group_id = aws_security_group.example_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "example_sg_egress_rule" {
+  security_group_id = aws_security_group.example_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 output "eip" {
